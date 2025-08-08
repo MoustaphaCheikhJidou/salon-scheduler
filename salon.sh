e3a04662eb93563e183d84b90755b943b607ae30
EL Moustapha Cheikh Jiddou <115373052+MoustaphaCheikhJidou@users.noreply.github.com>
2025-08-08 09:24:20 -0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,55 +1,75 @@
-#!/bin/bash
+#! /bin/bash
 
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
-echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+echo -e "\n ~~~~~ MY SALON ~~~~~\n"
 echo -e "Welcome to My Salon, how can I help you?\n"
 
-GET_SERVICES_ID() {
+CUSTOMER_NAME=""
+CUSTOMER_PHONE=""
+SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+
+CREATE_APPOINTMENT(){
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  CUST_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  CUST_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  SERVICE_NAME_FORMATTED=$(echo $SERVICE_NAME | sed 's/\s//g' -E)
+  CUST_NAME_FORMATTED=$(echo $CUST_NAME | sed 's/\s//g' -E)
+  echo -e "\nWhat time would you like your $SERVICE_NAME_FORMATTED, $CUST_NAME_FORMATTED?"
+  read SERVICE_TIME
+  INSERTED=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUST_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUST_NAME_FORMATTED."
+}
+
+MAIN_MENU(){
+echo -e "\nWhat's your phone number?"
+read CUSTOMER_PHONE
+
+HAVE_CUST=$($PSQL "SELECT customer_id, name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+
+if [[ -z $HAVE_CUST ]]
+then
+  echo -e "\nI don't have a record for that phone number, what's your name?"
+  read CUSTOMER_NAME
+
+  INSERTED=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+
+  CREATE_APPOINTMENT
+  else
+
+  CREATE_APPOINTMENT
+fi
+}
+
+
+
+LIST_SERVICES(){
   if [[ $1 ]]
   then
     echo -e "\n$1"
   fi
-  
-  LIST_SERVICES=$($PSQL "SELECT * FROM services")
-  echo "$LIST_SERVICES" | while read SERVICE_ID BAR SERVICE
+
+  echo "$SERVICES" | while read SERVICE_ID BAR NAME
   do
-    ID=$(echo $SERVICE_ID | sed 's/ //g')
-    NAME=$(echo $SERVICE | sed 's/ //g')
-    echo "$ID) $SERVICE"
+    echo "$SERVICE_ID) $NAME"
   done
+
   read SERVICE_ID_SELECTED
-  case $SERVICE_ID_SELECTED in
-    [1-5]) NEXT ;;
-        *) GET_SERVICES_ID "I could not find that service. What would you like today?" ;;
-  esac
-}
+  # if input is not a number
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  then
+    LIST_SERVICES "I could not find that service. What would you like today?"
+  else
+    HAVE_SERVICE=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
 
-NEXT() {
-  echo -e "\nWhat's your phone number?"
-  read CUSTOMER_PHONE
-  # CUSTOMER_PHONE_FORMATED=$(echo $CUSTOMER_PHONE | sed 's/[^0-9]*//g')
-  NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  CUSTOMER_NAME=$(echo $NAME | sed 's/ //g')
-  if [[ -z $NAME ]]
-  then
-    echo -e "\nI don't have a record for that phone number, what's your name?"
-    read CUSTOMER_NAME
-    NAME=$(echo $NAME | sed 's/ //g')
-    SAVED_TO_TABLE_CUSTOMERS=$($PSQL "INSERT INTO customers(name,phone) VALUES('$NAME','$CUSTOMER_PHONE')")
-  fi
-  
-  GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-  SERVICE_NAME=$(echo $GET_SERVICE_NAME| sed 's/ //g')
-  CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-  
-  echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
-  read SERVICE_TIME
-  SAVED_TO_TABLE_APPOINTMENTS=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-  if [[ $SAVED_TO_TABLE_APPOINTMENTS == "INSERT 0 1" ]]
-  then
-    echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+    if [[ -z $HAVE_SERVICE ]]
+    then
+      LIST_SERVICES "I could not find that service. What would you like today?"
+    else
+      MAIN_MENU
+    fi
   fi
 
 }
 
-GET_SERVICES_ID
+LIST_SERVICES
